532224f904f0b6cb980158f13174f1f795adae31
John Ansa <johnansa1@yahoo.com>
2022-02-10 10:46:04 +0100

    Add toggleCategory JS code

--- wwwroot/assets/js/main.js
+++ wwwroot/assets/js/main.js
@@ -7,24 +7,25 @@ const navItems = document.querySelectorAll('.nav-item');
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-  menuBtn.classList.toggle('close');
-  menu.classList.toggle('show');
-  menuNav.classList.toggle('show');
-  menuBranding.classList.toggle('show');
-  navItems.forEach((item) => item.classList.toggle('show'));
+	menuBtn.classList.toggle('close');
+	menu.classList.toggle('show');
+	menuNav.classList.toggle('show');
+	menuBranding.classList.toggle('show');
+	navItems.forEach((item) => item.classList.toggle('show'));
 }
 
 function setActiveMenu() {
-  var aObj = document.getElementById('main-nav').getElementsByTagName('a');
-  for (var i = 0; i < aObj.length; i++) {
-    if (aObj[i].href === document.location.href) {
-      aObj[i].className = 'current-menu';
-    }
-  }
+	var aObj = document.getElementById('main-nav').getElementsByTagName('a');
+	for (var i = 0; i < aObj.length; i++) {
+		if (aObj[i].href === document.location.href) {
+			aObj[i].className = 'current-menu';
+		}
+	}
 }
+
 window.onload = setActiveMenu;
 
-document.getElementById('year').innerHTML = new Date().getFullYear();
+document.getElementById('year').innerText = new Date().getFullYear();
 
 // Typewritter
 const typedTextSpan = document.querySelector('.typed-text');
@@ -38,52 +39,69 @@ let textArrayIndex = 0;
 let charIndex = 0;
 
 function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if (!cursorSpan.classList.contains('typing'))
-      cursorSpan.classList.add('typing');
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } else {
-    cursorSpan.classList.remove('typing');
-    setTimeout(erase, newTextDelay);
-  }
+	if (charIndex < textArray[textArrayIndex].length) {
+		if (!cursorSpan.classList.contains('typing'))
+			cursorSpan.classList.add('typing');
+		typedTextSpan.textContent +=
+			textArray[textArrayIndex].charAt(charIndex);
+		charIndex++;
+		setTimeout(type, typingDelay);
+	} else {
+		cursorSpan.classList.remove('typing');
+		setTimeout(erase, newTextDelay);
+	}
 }
 
 function erase() {
-  if (charIndex > 0) {
-    if (!cursorSpan.classList.contains('typing'))
-      cursorSpan.classList.add('typing');
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(
-      0,
-      charIndex - 1
-    );
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } else {
-    cursorSpan.classList.remove('typing');
-    textArrayIndex++;
-    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-    setTimeout(type, typingDelay + 1100);
-  }
+	if (charIndex > 0) {
+		if (!cursorSpan.classList.contains('typing'))
+			cursorSpan.classList.add('typing');
+		typedTextSpan.textContent = textArray[textArrayIndex].substring(
+			0,
+			charIndex - 1
+		);
+		charIndex--;
+		setTimeout(erase, erasingDelay);
+	} else {
+		cursorSpan.classList.remove('typing');
+		textArrayIndex++;
+		if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+		setTimeout(type, typingDelay + 1100);
+	}
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  // On DOM Load initiate the effect
-  if (textArray.length) setTimeout(type, newTextDelay + 250);
+	// On DOM Load initiate the effect
+	if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
 // Typewritter
 
 // Social Share
 
 function openPopupWindow(url, win) {
-  const w = 700;
-  const h = 600;
-  const y = win.top.outerHeight / 2 + win.top.screenY - h / 2;
-  const x = win.top.outerWidth / 2 + win.top.screenX - w / 2;
-  return win.open(
-    url,
-    'popup',
-    `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}`
-  );
+	const w = 700;
+	const h = 600;
+	const y = win.top.outerHeight / 2 + win.top.screenY - h / 2;
+	const x = win.top.outerWidth / 2 + win.top.screenX - w / 2;
+	return win.open(
+		url,
+		'popup',
+		`toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}`
+	);
 }
+
+document.getElementById('ca-selector').addEventListener('change', (e) => {
+	switch (e.target.value) {
+		case '1':
+			location.href = 'articles.html';
+			break;
+		case '2':
+			location.href = 'tutorials.html';
+			break;
+		case '3':
+			location.href = 'videos.html';
+			break;
+		default:
+			location.href = 'blog.html';
+	}
+});
